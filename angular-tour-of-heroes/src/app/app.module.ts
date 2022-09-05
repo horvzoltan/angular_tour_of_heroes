d5c5bb7a21cc3f87284a77566e329e503dd31f40
horvzoltan <zoltanhorvath0721@gmail.com>
2022-09-05 15:22:16 +0200

    feature: New changes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
@@ -36,6 +36,7 @@ import { SubmitFormComponent } from './submit-form/submit-form.component';
         FormsModule,
         AppRoutingModule,
         HttpClientModule,
+        ReactiveFormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-tour-of-heroes/src/app/submit-form/submit-form.component.ts
+++ angular-tour-of-heroes/src/app/submit-form/submit-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-submit-form',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-form.component.css']
 })
 export class SubmitFormComponent implements OnInit {
+  newBrewery = this.fb.group({
+    name: ['', Validators.required],
+    brewery_type: ['', Validators.required],
+    street: ['', Validators.required],
+    address_2: [''],
+    address_3: [''],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    county_province: [''],
+    postal_code: ['', Validators.required],
+    country: ['', Validators.required],
+    longitude: ['', Validators.required],
+    latitude: ['', Validators.required],
+    phone: ['', Validators.required],
+    website_url: ['',[Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(this.newBrewery.value);
+  }
 }
